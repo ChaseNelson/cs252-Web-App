8f6a3f2edd96ba2368b3b15b39ac26ab007ed98d
Chase Nelson <chase.a.nelson@gmail.com>
2018-04-24 09:46:19 -0400

    handle firebase creds better

---
+++ public/javascript/firebaseCred.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyBrJCE5BVlXicolinRD8Vl5omPA0wiq4R4",
+    authDomain: "cs252-web-app.firebaseapp.com",
+    databaseURL: "https://cs252-web-app.firebaseio.com",
+    projectId: "cs252-web-app",
+    storageBucket: "cs252-web-app.appspot.com",
+    messagingSenderId: "225077387827"
+  };
+firebase.initializeApp(config);
--- public/javascript/postMessage.js
+++ public/javascript/postMessage.js
@@ -1,13 +1,3 @@
-const config = {
-    apiKey: "AIzaSyBrJCE5BVlXicolinRD8Vl5omPA0wiq4R4",
-    authDomain: "cs252-web-app.firebaseapp.com",
-    databaseURL: "https://cs252-web-app.firebaseio.com",
-    projectId: "cs252-web-app",
-    storageBucket: "cs252-web-app.appspot.com",
-    messagingSenderId: "225077387827"
-  };
-firebase.initializeApp(config);
-
 function postData(uid) {
   const message   = document.getElementById('messageContent').value;
   const timestamp = Date.now();
